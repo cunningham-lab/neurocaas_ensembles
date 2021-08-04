2544ed6c35ad549c295773b3c7dc0ec16831675c
cellistigs <ta2507@columbia.edu>
2021-08-04 15:17:08 -0400

    added compute_influence confidence refactoring.

--- scripts/compute_influence_confidence.py
+++ scripts/compute_influence_confidence.py
@@ -19,19 +19,19 @@ modelpaths = "ensemble-model{i}-2030-01-0{i}"
 @click.option("--groundtruth",default = "../data/ibl/ibl1_true_xy_all_918pm.mat")
 @click.option("--partperm",default = "ibl",help="if we need to permute the labels of the groundtruth before comparing.")
 @click.option("--labellist",help = "path to pickled list of labels and indicator of if they are outliers or not.",default = "../data/ibl/ordered_classified_list")
-@click.option("--basefolder",default = "/home/ubuntu/july_data")
-@click.option("--resultsfolder",help="path to folder containing json files of outputs.",default = "../data/ibl/consensus_performance")
-def main(video_name,groundtruth,partperm,labellist,basefolder,resultsfolder):
+@click.option("--ensemblesfolder",default = "/home/ubuntu/july_data")
+def main(video_name,groundtruth,partperm,labellist,ensemblesfolder):
     """Runs after calculate_consensus script. Takes the performance estimates and ensembles from that data, and determines properties of the data from them.  
 
     """
+    idstring = os.path.splitext(video_name)[0]+os.path.basename(os.path.normpath(ensemblesfolder))
     if partperm == "ibl":
         partperm = np.array([1,3,0,2]) # permute parts before comparing
     else:
         partperm = None
 
     ## Step 0: First get all diffs:     
-    ensembles = [parse_modelname(m,labellist,basefolder) for m in os.listdir(basefolder) if parse_modelname(m,labellist,basefolder) is not None]
+    ensembles = [parse_modelname(m,labellist,ensemblesfolder) for m in os.listdir(ensemblesfolder) if parse_modelname(m,labellist,ensemblesfolder) is not None]
     gt = ensembles[0]["models"][0].get_groundtruth(groundtruth,partperm =partperm )
 
     for e in ensembles:
@@ -40,7 +40,7 @@ def main(video_name,groundtruth,partperm,labellist,basefolder,resultsfolder):
             diff = m.get_groundtruth_confidence(groundtruth,video_name,range(1001),partperm = [1,3,0,2])
             e["modeldiffs"]["model{}".format(mi+1)] = diff
             
-    joblib.dump(ensembles,os.path.join(scriptdir,"script_outputs","confidence_data"))
+    joblib.dump(ensembles,os.path.join(scriptdir,"script_outputs","confidence_data_{}".format(idstring)))
 
 
 if __name__ == "__main__":
